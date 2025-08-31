a6c92089688d96d0bf96e35be36fbe1c55fe4f91
nishan-t26 <nishantsharma7408@gamil.com>
2025-08-31 17:55:12 +0530

    bootstrap added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Main from './components/main'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,10 +1,16 @@
 const Main=()=>{
     
 return(<>
-    <div className="">first</div>
-    <div>Second</div>
-    <div>Third</div>
-    <div>Forth</div>    
+<div className="wrapper">
+    <div className="navbar row">
+        <div className="col-md-3">first</div>
+        <div className="col-md-3">Second</div>
+        <div className="col-md-3">Third</div>
+        <div className="col-md-3">Forth</div>    
+    </div>
+</div>
+
+   
 </>)
 }
 export default Main
